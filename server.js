97180464436fc826738c80f076c321f2e1d31e8a
Sean Baildon <sean@sbaildon.com>
2014-02-03 11:08:07 +0000

    include basic socket.io usage

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(80);
 
 app.use('/libs', express.static(__dirname + '/libs'));
 app.use('/js', express.static(__dirname + '/js'));
@@ -11,6 +15,11 @@ app.get('/', function(req, res) {
 	res.sendfile('index.html');
 });
 
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
 
-app.listen(80);
 console.log('Server running');
