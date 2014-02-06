d1e771f8baefd115432487d82e93b5f2a13cb039
Sean Baildon <sean@sbaildon.com>
2014-02-06 16:10:25 +0000

    included next refresh counter into status panel

--- js/controllers/statusPanelController.js
+++ js/controllers/statusPanelController.js
@@ -7,7 +7,9 @@ statusPanelController.controller('statusPanelController', function($scope, $http
 	$scope.errorText = "N/A";
 
 	$scope.pollingRate;
-	$scope.pollingRateUnit;
+
+	$scope.refreshTimer = 10;
+	$scope.queryInProgress = false;
 
 	$scope.lastUpdate;
 	$scope.lastUpdateTime;
@@ -15,26 +17,34 @@ statusPanelController.controller('statusPanelController', function($scope, $http
 
 	$scope.refreshTick = function() {
 		$scope.lastUpdateVerbose = moment($scope.lastUpdate).fromNow();
+		if($scope.refreshTimer == 0 && !$scope.queryInProgress) {
+			$scope.refreshTimer = 10;
+			$scope.queryMetadata();
+		} else if ($scope.refreshTimer != 0) {
+			$scope.refreshTimer = $scope.refreshTimer - 1;
+		}
 	};
 
 	$scope.queryMetadata = function() {
+		$scope.queryInProgress = true;
 		$http({
 			method: 'GET',
-			url: 'https://api.xively.com/v2/feeds/34780663',
+			url: 'https://api.xively.com/v2/feeds/34780663?datastreams=polling-rate',
 			headers: {
 				'X-ApiKey': 'TuQQMiTAvUUg7qVwoQQmCi0i4CyGIcmmzKKDshENxCxibDaD',
 				'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
 				'Pragma': 'no-cache'
 			}
 		}).success(function(data) {
-			$scope.pollingRate = $scope.calculatePollingTime(parseInt(data.datastreams[4].current_value));
-
+			$scope.pollingRate = $scope.calculatePollingTime(parseInt(data.datastreams[0].current_value));
 			$scope.lastUpdate = data.updated;
 			$scope.lastUpdateTime = moment($scope.lastUpdate).format('HH:mm:ss');
 			$scope.lastUpdateVerbose = moment($scope.lastUpdate).fromNow();
+			$scope.queryInProgress = false;
 		}).error(function(data) {
 			$scope.pollingRate = $scope.errorText;
 			$scope.lastUpdateTime = $scope.errorText;
+			$scope.queryInProgress = false;
 		});
 	};
 
@@ -42,25 +52,21 @@ statusPanelController.controller('statusPanelController', function($scope, $http
 		var minutes;
 		minutes = moment.duration(data).asMinutes();
 		if (minutes >= 1) {
-			$scope.pollingRateUnit = "mins";
-			return minutes;
+			return {'value': minutes, 'unit': 'mins' };
 		}
 
 		var seconds;
 		seconds = moment.duration(data).asSeconds();
 		if (seconds >= 1) {
-			$scope.pollingRateUnit = "secs";
-			return seconds;
+			return {'value': seconds, 'unit': 'secs' };
 		}
 
-		$scope.pollingRateUnit = "ms";
-		return data;
+		return {'value': data, 'unit': 'ms' };
 	}
 
 	$document.ready(function() {
 		$scope.queryMetadata();
 	});
 
-	$interval($scope.queryMetadata, 10000);
-	$interval($scope.refreshTick, 3000);
+	$interval($scope.refreshTick, 1000);
 });
