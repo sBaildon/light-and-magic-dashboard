02ce7d4b96d345a2214e7cc8cabce896f5f1e691
Sean Baildon <sean@sbaildon.com>
2014-02-07 17:15:35 +0000

    removed references to socket.io

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
 
-server.listen(80);
+app.listen(80);
 
 app.use('/libs', express.static(__dirname + '/libs'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
-
-require('./requester.js');
+app.use('/img', express.static(__dirname + '/img'));
 
 app.get('/', function(req, res) {
 	res.sendfile('index.html');
-});
-
-io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
 });
 
 console.log('Server running');
