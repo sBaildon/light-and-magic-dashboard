3fb99cda2046102f1b365fb70e04a08bd6c53d47
Sean Baildon <sean@sbaildon.com>
2014-02-08 20:41:42 +0000

    included .then() promise on HTTP for more semantic code

--- js/controllers/statusPanelController.js
+++ js/controllers/statusPanelController.js
@@ -12,13 +12,11 @@ statusPanelController.controller('statusPanelController', function($scope, $http
 	$scope.queryInProgress = false;
 
 	$scope.lastUpdate;
-	$scope.lastUpdateTime;
-	$scope.lastUpdateVerbose;
 
 	$scope.refreshTick = function() {
 		$scope.lastUpdateVerbose = moment($scope.lastUpdate).fromNow();
+
 		if($scope.refreshTimer == 0 && !$scope.queryInProgress) {
-			$scope.refreshTimer = 10;
 			$scope.queryMetadata();
 		} else if ($scope.refreshTimer != 0) {
 			$scope.refreshTimer = $scope.refreshTimer - 1;
@@ -29,26 +27,30 @@ statusPanelController.controller('statusPanelController', function($scope, $http
 		$scope.queryInProgress = true;
 		$http({
 			method: 'GET',
-			url: 'https://api.xively.com/v2/feeds/34780663?datastreams=polling-rate',
+			url: 'https://api.xively.com/v2/feeds/34780663?datastreams=polling-rate,wifi-ssid',
 			headers: {
 				'X-ApiKey': 'TuQQMiTAvUUg7qVwoQQmCi0i4CyGIcmmzKKDshENxCxibDaD',
 				'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
 				'Pragma': 'no-cache'
 			}
 		}).success(function(data) {
-			$scope.pollingRate = $scope.calculatePollingTime(parseInt(data.datastreams[0].current_value));
+			$scope.pollingRate = $scope.calculatePollingTime(data.datastreams[0].current_value);
+			$scope.wifiSSID = data.datastreams[1].current_value;
 			$scope.lastUpdate = data.updated;
 			$scope.lastUpdateTime = moment($scope.lastUpdate).format('HH:mm:ss');
 			$scope.lastUpdateVerbose = moment($scope.lastUpdate).fromNow();
-			$scope.queryInProgress = false;
 		}).error(function(data) {
 			$scope.pollingRate = $scope.errorText;
 			$scope.lastUpdateTime = $scope.errorText;
+		}).then(function() {
+			$scope.refreshTimer = 10;
 			$scope.queryInProgress = false;
 		});
 	};
 
 	$scope.calculatePollingTime = function(data) {
+		data = parseInt(data);
+		
 		var minutes;
 		minutes = moment.duration(data).asMinutes();
 		if (minutes >= 1) {
